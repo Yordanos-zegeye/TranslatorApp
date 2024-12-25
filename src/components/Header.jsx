1c5509689b8fb1b0e0bb17beecbd3f14f5687f44
Yordanos-zegeye <yordanoszegeye24@gmail.com>
2024-12-25 13:48:33 +0300

    edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between gap-4 p-4">
       <h1 className="font-medium">
-        Free<span className="text-blue-400">Scribe</span>
+        Word<span className="text-teal-400">Smith</span>
       </h1>
-      <button className="flex items-center gap-2 specialBtn px-4 py-2 rounded-lg text-blue-400">
+      <button className="flex items-center gap-2 specialBtn px-4 py-2 rounded-lg text-teal-400">
         <p>New</p>
         <i className="fa-solid fa-plus"></i>
       </button>
